a0d55b049beefab01251c1547996c9866386dce6
gur 123 <g441724@gmail.com>
2024-02-27 12:37:42 +0200

    Create class Proudct and a Main

---
+++ Store/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Store.Enums;
+
+namespace Store
+{
+
+    public class Product : BaseEntity
+    {
+
+        #region -- Proprites --
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public bool IsInStock { get; set; }
+        public string ParentCategoryId { get; set; } // the id of Parent Category, if empty it has no category.
+        #endregion
+
+        #region -- constractor --
+        public Product() : base() // Empty Ctor with defualt values.
+        {
+            Name = "Default Product";
+            Price = 0.0m;
+            IsInStock = false;
+            ParentCategoryId = string.Empty;
+        }
+        #endregion
+
+
+        public override string ToString() // return Prduct properties as text
+        {
+            return $"Name: {Name}, Price: {Price}, IsInStock: {IsInStock}, ParentCategoryId: {ParentCategoryId}";
+        }
+    }
+}
---
+++ main/Program.cs
@@ -0,0 +1,14 @@
+﻿using Store;
+
+
+namespace main
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseEntity p1 = new Product();
+            Console.WriteLine(p1);
+        }
+    }
+}
